015c887cb94a8a7908b0fe8f5b17e3a2d12fb6d3
Satya <satyaranjanb821@gmail.com>
2025-08-07 20:08:26 +0530

    Update index.js

--- index.js
+++ index.js
@@ -91,7 +91,7 @@ app.use(notFound);
 app.use(errorHandler);
 
 // Start server
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 5173;
 const server = app.listen(PORT, () => {
   console.log(`✅ Server running on port ${PORT}`);
 });
